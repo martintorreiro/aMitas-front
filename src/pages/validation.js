1cef6447f65c10a12344fbb5a66fbbeb9c24512c
[martintorreiro] <[martin_vga4@hotmail.com]>
2022-12-14 15:27:02 +0100

    commit

--- src/pages/validation.js
+++ src/pages/validation.js
@@ -1,56 +1,49 @@
-/* import { Link } from "react-router-dom";
-import { useLogin } from "../hooks/useLogin"; */
-
 import { useState } from "react";
 import { useLocation } from "react-router-dom";
 import { validateService } from "../service";
 import { useNavigate } from "react-router-dom";
 
-
 export const Validate = () => {
-  const [inputCode,setInputCode] = useState("")
-  const [error,setError]= useState("")
-  const [loading,setLoading]= useState("")
+  const [inputCode, setInputCode] = useState("");
+  const [error, setError] = useState("");
+  const [loading, setLoading] = useState(false);
   const { state } = useLocation();
-  console.log("state",state)
+  console.log("state", state);
   const navigate = useNavigate();
 
-const handleSubmit = async  (e) => {
-
-e.preventDefault()
-
-try { 
-  const resValidation = await  validateService(state.email,inputCode)
-  navigate("/login")
-} catch (error) {
-  console.log("error",error)
-  setError(error.message)
-}
-
-
-
-}
-
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    setLoading(true);
+    try {
+      const resValidation = await validateService(state.email, inputCode);
+      setLoading(false);
+      navigate("/login");
+    } catch (error) {
+      console.log("error", error);
+      setLoading(false);
+      setError(error.message);
+    }
+  };
 
   return (
     <section className="register">
       <h2>Pagina de validacion.</h2>
-      <form className="register-form"  onSubmit={handleSubmit} >
+      <form className="register-form" onSubmit={handleSubmit}>
         <fieldset>
           <input
             type="text"
             id="validationCode"
             name="validationCode"
             placeholder="Validation code"
-            onChange={(e)=>setInputCode(e.target.value)}
+            onChange={(e) => setInputCode(e.target.value)}
             required
           />
 
           <div className="button-container">
             <button className="button">Validar</button>
           </div>
-            {loading ? <p>Loading</p> : <></>}
-          {error ? <p>{error}</p> : <></>} 
+          {loading ? <p>Loading</p> : <></>}
+          {error ? <p>{error}</p> : <></>}
         </fieldset>
       </form>
     </section>
--- src/pages/work-page.js
+++ src/pages/work-page.js
@@ -4,7 +4,13 @@ import { Cuentas } from "../components/calc-example";
 export const WorkPage = () => {
   return (
     <section>
-      <h2>Pagina de prueba de calculo</h2>
+      <section>
+        <h2>Prueba una lista ya creada</h2>
+      </section>
+      <section>
+        <h2>Crea tu propia lista</h2>
+      </section>
+
       <Cuentas></Cuentas>
     </section>
   );

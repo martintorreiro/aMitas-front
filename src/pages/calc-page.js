03fc35558a96e1ef9dc4289f44863ff49406f2b6
[martintorreiro] <[martin_vga4@hotmail.com]>
2022-12-26 20:47:25 +0100

    borrado de datos residuales

--- src/pages/calc-page.js
+++ src/pages/calc-page.js
@@ -13,14 +13,9 @@ export const Cuentas = () => {
         <h3>Selecciona una opcion</h3>
         <section className="selection-box" onClick={(e) => setMode("create")}>
           <h4>Crear pagina</h4>
-          {/* <p>Crea una nueva pagina de cuentas de ejemplo en la que trabajar</p> */}
         </section>
         <section className="selection-box" onClick={(e) => setMode("test")}>
           <h4>Probar ejemplo</h4>
-          {/*  <p>
-            Prueba una plantilla de ejemplo en la que podras añadir gastos y
-            hacerte una idea de como funciona
-          </p> */}
         </section>
       </section>
       {mode === "create" ? <CreateCalc></CreateCalc> : <></>}
